492efe32805c24f34ba260a778f545c9f4d78009
梚辰 <1911308683@qq.com>
2024-06-11 03:33:22 +0800

    add readme license

--- Assets/StarryFramework/Framework/Runtime/Base/FrameworkSettings.cs
+++ Assets/StarryFramework/Framework/Runtime/Base/FrameworkSettings.cs
@@ -13,20 +13,20 @@ namespace StarryFramework
 
         internal int FrameworkSceneID = -1;
 
-        [Header("游戏框架的Log等级")] [Space(5)] 
+        [Header("Log Level")] [Space(5)] 
         [SerializeField] internal FrameworkDebugType debugType = FrameworkDebugType.Normal;
         
-        [Header("框架内部事件")] [Tooltip("框架内部事件被触发时，是否会同时触发外部同名事件")] [Space(5)] 
+        [Header("Framework Internal Event")] [Tooltip("框架内部事件被触发时，是否会同时触发外部同名事件。When an internal framework event is triggered, whether an external event with the same name will also be triggered simultaneously")] [Space(5)] 
         [SerializeField] internal bool InternalEventTrigger = true;
         
-        [Header("初始场景加载")] [Tooltip("游戏启动加载的初始场景，如果是GameFramework则不加载")] [Space(5)] 
+        [Header("Initial Scene Load")] [Tooltip("游戏启动加载的初始场景，如果是GameFramework则不加载。If the initial scene to load is the GameFramework, then it does nothing")] [Space(5)] 
         [SerializeField] [SceneIndex] internal int StartScene = 0;
 
-        [Tooltip("初始场景加载是否启用默认动画")]
+        [Tooltip("初始场景加载是否启用默认动画。Whether the initial scene loading enable the default animation")]
         [SerializeField] internal bool StartSceneAnimation = false;
         
-        [Header("游戏框架启用的模块")] [Space(5)]
-        [Tooltip("游戏框架各模块是否启用以及优先级，越靠近列表前端优先级越高")]
+        [Header("Modules Enabled")] [Space(5)]
+        [Tooltip("游戏框架各模块是否启用以及优先级，越靠近列表前端优先级越高。Whether each module of the game framework is enabled and its priority, with higher priority given to those closer to the top of the list.")]
         [SerializeField] internal List<ModuleType> modules= new List<ModuleType>();
 
         internal bool ModuleInUse(ModuleType type)

8316e36886500f373a15c698471c21e55fbca85c
starryforest-ymxk <1911308683@qq.com>
2026-02-27 15:07:04 +0800

    Update AssemblyInfo

--- Assets/StarryFramework/Extensions/Runtime/Assembly.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("StarryFramework.Extensions.Editor")]
-namespace StarryFramework.Extentions { }
--- Assets/StarryFramework/Runtime/AssemblyInfo.cs
+++ Assets/StarryFramework/Runtime/AssemblyInfo.cs
@@ -3,4 +3,3 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("StarryFramework.Editor")]
 [assembly: InternalsVisibleTo("StarryFramework.Extensions")]
 [assembly: InternalsVisibleTo("StarryFramework.Extensions.Editor")]
-namespace StarryFramework { }
